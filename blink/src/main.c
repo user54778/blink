6dc1dec4974b344253b55c4fc5b773341ae13bc8
Ahron Pollak <ahronrocks51@gmail.com>
2025-11-19 13:56:46 -0500

    basic makefile setup with bear

---
+++ blink/src/main.c
@@ -0,0 +1,57 @@
+#include "stm32f0xx_hal.h"
+
+void SystemClockConfig(void);
+void ErrorHandler(void);
+
+int main() {
+  // Initialize HAL; reset peripherals, init flash interface and systick
+  HAL_Init();
+
+  // Configure system clock
+  SystemClockConfig();
+
+  // init peripherals here
+  // we need to a) init gpio ports, b) configure LD2 LED
+
+  while (1) {
+  }
+}
+
+//
+void SystemClockConfig() {
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+   * in the RCC_OscInitTypeDef structure.
+   */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+    ErrorHandler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+   */
+  RCC_ClkInitStruct.ClockType =
+      RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+    ErrorHandler();
+  }
+}
+
+// Custom Error_Handler
+void ErrorHandler(void) {
+  __disable_irq();
+  while (1) {
+  }
+}
